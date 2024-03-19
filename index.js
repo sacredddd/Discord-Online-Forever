952187f4c4534aef9719281221f68b82e60d274c
sacredddd <132947372+sacredddd@users.noreply.github.com>
2024-03-19 09:19:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Eris = require("eris");
 const keep_alive = require('./keep_alive.js')
 
 // Replace TOKEN with your bot account's token
-const bot = new Eris(process.env.token);
+const bot = new Eris(process.env.:MTIxOTQwNjA5MzkwMjU0NDg5Nw.GCV2V9.G8rNLv9GaQV4b8BwmIkGRdXZ6BK1MKF-cB2W8c);
 
 bot.on("error", (err) => {
   console.error(err); // or your preferred logger
